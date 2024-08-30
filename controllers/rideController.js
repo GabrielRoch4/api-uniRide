45ee392738282587786d159059293ba2e6ec785d
Gabriel Rocha <111160701+GabrielRoch4@users.noreply.github.com>
2024-08-30 14:45:32 -0300

    Crud ride

--- controllers/rideController.js
+++ controllers/rideController.js
@@ -35,6 +35,46 @@ export const getAllRides = async (_, res) => {
     }
 };
 
+export const getRideById = async (req, res) => {
+    const { id } = req.params; // ID da carona a ser recuperada
+
+    try {
+        // Recupera a carona pelo ID
+        const ride = await prisma.ride.findUnique({
+            where: { Id: id },
+            include: {
+                User: {
+                    select: {
+                        Id: true,
+                        Nome: true,
+                        Sobrenome: true,
+                        Email: true,
+                        Telefone: true,
+                        Genero: true
+                    }
+                }
+            }
+        });
+
+        if (!ride) {
+            return res.status(404).json({
+                statusCode: 404,
+                message: "Carona não encontrada!"
+            });
+        }
+
+        return res.status(200).json({
+            statusCode: 200,
+            ride
+        });
+    } catch (error) {
+        return res.status(500).json({
+            statusCode: 500,
+            message: error.message
+        });
+    }
+};
+
 export const createRide = async (req, res) => {
     const userId = req.userId;
 
@@ -76,4 +116,109 @@ export const createRide = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 };
+
+export const updateRide = async (req, res) => {
+    const userId = req.userId;
+    const { id } = req.params; // ID da carona a ser atualizada
+    const { Origem, Destino } = req.body;
+
+    try {
+        // Recupera a carona existente
+        const ride = await prisma.ride.findUnique({
+            where: { Id: id },
+            include: {
+                User: true // Inclui o usuário que criou a carona
+            }
+        });
+
+        if (!ride) {
+            return res.status(404).json({
+                statusCode: 404,
+                message: "Carona não encontrada!"
+            });
+        }
+
+        // Verifica se o usuário autenticado é o mesmo que criou a carona
+        if (ride.User.Id !== userId) {
+            return res.status(403).json({
+                statusCode: 403,
+                message: "Você não tem permissão para atualizar esta carona!"
+            });
+        }
+
+        // Atualiza os dados da carona
+        const updateData = {};
+
+        if (Origem) updateData.Origem = Origem;
+        if (Destino) updateData.Destino = Destino;
+
+        if (Object.keys(updateData).length === 0) {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Nenhum dado para atualizar foi fornecido!"
+            });
+        }
+
+        const updatedRide = await prisma.ride.update({
+            where: { Id: id },
+            data: updateData,
+        });
+
+        return res.status(200).json({
+            statusCode: 200,
+            message: "Carona atualizada com sucesso!",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            statusCode: 500,
+            message: error.message
+        });
+    }
+};
+
+export const deleteRide = async (req, res) => {
+    const userId = req.userId;
+    const { id } = req.params; // ID da carona a ser excluída
+
+    try {
+        // Recupera a carona existente
+        const ride = await prisma.ride.findUnique({
+            where: { Id: id },
+            include: {
+                User: true // Inclui o usuário que criou a carona
+            }
+        });
+
+        if (!ride) {
+            return res.status(404).json({
+                statusCode: 404,
+                message: "Carona não encontrada!"
+            });
+        }
+
+        // Verifica se o usuário autenticado é o mesmo que criou a carona
+        if (ride.User.Id !== userId) {
+            return res.status(403).json({
+                statusCode: 403,
+                message: "Você não tem permissão para excluir esta carona!"
+            });
+        }
+
+        // Exclui a carona
+        await prisma.ride.delete({
+            where: { Id: id }
+        });
+
+        return res.status(200).json({
+            statusCode: 200,
+            message: "Carona excluída com sucesso!"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            statusCode: 500,
+            message: error.message
+        });
+    }
+};
+
 
--- routes/rideRoutes.js
+++ routes/rideRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createRide, getAllRides } from "../controllers/rideController.js";
+import { createRide, deleteRide, getAllRides, getRideById, updateRide } from "../controllers/rideController.js";
 import authController from "../controllers/authController.js";
 
 const rideRoutes = express.Router();
 
 rideRoutes.get("/rides", getAllRides);
+rideRoutes.get("/rides/:id", getRideById);
 rideRoutes.post("/rides/create", authController.verifyToken, createRide);
+rideRoutes.put("/rides/update/:id", authController.verifyToken, updateRide);
+rideRoutes.delete("/rides/delete/:id", authController.verifyToken, deleteRide);
 
 export default rideRoutes;
