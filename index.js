532910bd82509f981c98dfa80f837d0844951916
Gabriel Rocha <111160701+GabrielRoch4@users.noreply.github.com>
2024-09-04 10:33:38 -0300

    Testes variáveis de ambiente

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ import router from "./routes/router.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*', // Substitua pelo seu domínio
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use("/", router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const SECRET = process.env.SECRET;
+const DATABASE_URL = process.env.DATABASE_URL;
+
 app.listen(PORT, () => {
+  console.log(`"DATABASE_URL:", ${DATABASE_URL}`);
+  console.log(`"SECRET:", ${SECRET}`);
   console.log(`Servidor rodando na porta ${PORT}`);
 });
